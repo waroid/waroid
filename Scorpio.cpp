95e2c3aeaf3439e2bfd932af3f2c033a23d2e6c4
mirime <kmirime@gmail.com>
2015-12-07 15:40:38 +0900

    modified

--- Scorpio.cpp
+++ Scorpio.cpp
@@ -12,24 +12,35 @@
 namespace SCORPIO
 {
 	const int DIRECTION_COUNT = 11;
-	const float DIRECTION_DATA[DIRECTION_COUNT][4] =
+	const float DIRECTION_DATA[DIRECTION_COUNT][3] =
 	{
 	//move matrix
 	//[-1.0, -1.732, 1.0]
 	//[-1.0, 1.732, 1.0]
 	//[2.0, 0.0, 1.0]
 	//A:motor0, A:motor1, B:motor0, Scale
-	{ 0.0, 0.0, 0.0, 1.0 },			//idle
-	{ -1.732, 1.732, 0.0, 1.732 },	//forward
-	{ -2.732, 0.732, 2.0,  2.732 },	//right forward
-	{ -1.0, -1.0, 2.0, 2.0 },		//right
-	{ 0.732, -2.732, 2.0, 2.732 },	//right backward
-	{ 1.732, -1.732, 0, 1.732 },	//backward
-	{ 2.732, -0.732, -2.0, 2.732 },	//left backward
-	{ 1.0, 1.0, -2.0, 2.0 },		//left
-	{ -0.732, 2.732, -2.0, 2.732 },	//left forward
-	{ -1.0, -1.0, -1.0, 1.0 },		//turn right
-	{ 1.0, 1.0, 1.0, 1.0 },			//turn left
+	//{ 0.0, 0.0, 0.0, 1.0 },			//idle
+	//{ -1.732, 1.732, 0.0, 1.732 },	//forward
+	//{ -2.732, 0.732, 2.0,  2.732 },	//right forward
+	//{ -1.0, -1.0, 2.0, 2.0 },		//right
+	//{ 0.732, -2.732, 2.0, 2.732 },	//right backward
+	//{ 1.732, -1.732, 0, 1.732 },	//backward
+	//{ 2.732, -0.732, -2.0, 2.732 },	//left backward
+	//{ 1.0, 1.0, -2.0, 2.0 },		//left
+	//{ -0.732, 2.732, -2.0, 2.732 },	//left forward
+	//{ -1.0, -1.0, -1.0, 1.0 },		//turn right
+	//{ 1.0, 1.0, 1.0, 1.0 },			//turn left
+	{ 0.0, 0.0, 0.0 },			//idle
+	{ -0.99, 0.995, 0.0 },		//forward
+	{ -0.99, 0.316, 0.746 },	//right forward
+	{ -0.5, -0.5, 0.995 },		//right
+	{ 0.316, -0.99, 0.746 },	//right backward
+	{ 0.995, -0.99, 0 },		//backward
+	{ 0.995, -0.3, -0.733 },	//left backward
+	{ 0.509, 0.509, -0.99 },	//left
+	{ -0.3, 0.995, -0.733 },	//left forward
+	{ -0.99, -0.99, -0.99 },	//turn right
+	{ 0.995, 0.995, 0.995 },	//turn left
 	};
 }
 using namespace SCORPIO;
@@ -87,10 +98,11 @@ void Scorpio::onMove(int data0, int data1)
 	if (speed < 0) speed = 0;
 	else if (speed >= ROBOT_MAX_SPEED) speed = 0;
 
-	float motor[3] = { 0.0, 0.0, 0.0 };
-	for (int i = 0; i < 3; ++ i)
+	float motor[3] =
+	{ 0.0, 0.0, 0.0 };
+	for (int i = 0; i < 3; ++i)
 	{
-		motor[i] = (DIRECTION_DATA[dir][i] / DIRECTION_DATA[dir][3]) * g_speedScale[speed];
+		motor[i] = DIRECTION_DATA[dir][i];
 	}
 	m_picoBorgReverse0.move(motor[0], motor[1]);
 	m_picoBorgReverse1.move(motor[2], 0);
